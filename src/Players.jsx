1a3298775a465d3abaf31f66bdeef4ea721f0e12
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-09-08 11:46:12 +0600

    event

--- src/Players.jsx
+++ src/Players.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react"
+
 export default function Players () {
+    const [count, setCount] = useState(0);
+    const handelCount = () =>{
+        const newCount = count+1;
+        setCount(newCount)
+    }
+    const handelReduce = () =>{
+        const newCount = count -1;
+        setCount(newCount)
+    }
+    const teamStyle= {
+        border: '2px solid yellow',
+        margin: '15px',
+        padding: '15px',
+        borderRadius: '20px'
+    }
     return (
-        <div>
-            <h3>Players :</h3>
+        <div style={teamStyle}>
+            <h3>Players : {count}</h3>
+            <button onClick={handelCount}>add</button>
+            <button onClick={handelReduce}>Reduce</button>
         </div>
     )
 }
